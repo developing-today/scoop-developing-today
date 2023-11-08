6f67a22a55e679a5f0483bd2db6d817ab513032a
Drewry Pope <drewrypope@gmail.com>
2023-11-08 03:18:38 -0600

    Update auto-pr.ps1
    Signed-off-by: Drewry Pope <drewrypope@gmail.com>

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "kodybrown/scoop-nirsoft:master"
+    [String]$upstream = "developing-today/scoop-developing-today:main"
 )
 
 if(!$env:SCOOP_HOME) { $env:SCOOP_HOME = resolve-path (split-path (split-path (scoop which scoop))) }
